74d4984239b06ff1faf563ffd29ce89136e39f78
LuZhang-Lou <lzhang3@andrew.cmu.edu>
2016-10-09 01:19:54 -0400

    update test

--- src/main.cpp
+++ src/main.cpp
@@ -83,48 +83,57 @@ class LockFreeCircularArray {
 
 };
 
-void producer(LockFreeCircularArray<uint32_t >* queue, uint32_t start, uint32_t end) {
+void producer(std::atomic<int>* guard, LockFreeCircularArray<uint32_t >* queue, uint32_t start, uint32_t end) {
   bool ret;
   for (uint32_t i = start; i < end; ++i) {
     do {
       ret = queue->push(i);
     } while (!ret);
   }
+  (*guard)++;
 }
 
-void consumer(LockFreeCircularArray<uint32_t >* queue, std::set<uint32_t >* set) {
-  while (queue->size() != 0) {
+void consumer(std::atomic<int>* prod_token, std::atomic<int>* con_token, LockFreeCircularArray<uint32_t >* queue, std::set<uint32_t >* set) {
+  while (queue->size() != 0  || *prod_token != 4) {
     bool res;
     uint32_t val = queue->pop(&res);
     if (res) {
       set->insert(val);
     }
   }
+  (*con_token)++;
 }
 
 int main() {
   std::cout << "Hello, LockFree!" << std::endl;
   LockFreeCircularArray<uint32_t > queue;
-  MutexSet<uint32_t> set;
+  std::atomic<int> prod_token(0);
+  std::atomic<int> con_token(0);
   std::vector<std::set<uint32_t> > sets(5);
-  std::thread prod1(producer, &queue, 0, 10000);
-  std::thread prod2(producer, &queue, 10000, 20000);
-  std::thread consumer1(consumer, &queue, &sets[0]);
-  std::thread consumer2(consumer, &queue, &sets[1]);
-  std::thread prod3(producer, &queue, 20000, 30000);
-  std::thread prod4(producer, &queue, 30000, 40000);
-  std::thread consumer3(consumer, &queue, &sets[2]);
-  std::thread consumer4(consumer, &queue, &sets[3]);
-  std::thread consumer5(consumer, &queue, &sets[4]);
-  prod1.join();
-  consumer3.join();
-  prod2.join();
-  consumer1.join();
-  consumer2.join();
-  prod3.join();
-  prod4.join();
-  consumer4.join();
-  consumer5.join();
+  std::thread prod1(producer, &prod_token, &queue, 0, 10000);
+  std::thread prod2(producer, &prod_token, &queue, 10000, 20000);
+  std::thread prod3(producer, &prod_token, &queue, 20000, 30000);
+  std::thread prod4(producer, &prod_token, &queue, 30000, 40000);
+
+  std::thread consumer1(consumer, &prod_token, &con_token, &queue, &sets[0]);
+  std::thread consumer3(consumer, &prod_token, &con_token, &queue, &sets[2]);
+  std::thread consumer4(consumer, &prod_token, &con_token, &queue, &sets[3]);
+  std::thread consumer5(consumer, &prod_token, &con_token, &queue, &sets[4]);
+  std::thread consumer2(consumer, &prod_token, &con_token, &queue, &sets[1]);
+
+  prod1.detach();
+  prod2.detach();
+  prod3.detach();
+  prod4.detach();
+
+  consumer1.detach();
+  consumer2.detach();
+  consumer3.detach();
+  consumer4.detach();
+  consumer5.detach();
+  while (con_token != 5) {
+    ;
+  }
   std::cout << "verifying..: " << std::endl;
   for (uint32_t i = 0; i < 40000u; ++i) {
     uint8_t j = 0;
